5052f1dc386586bd83dc644fbea40a991c7c3087
Hazem <hazemm913@gmail.com>
2023-07-17 07:41:40 +0300

    add ajax to pics gallery

--- core/urls.py
+++ core/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('advertising/', views.advertising, name='advertising'),
     path('architecture/', views.architecture, name='architecture'),
+    path('fetch_pics/<str:type>',views.fetch_pics,name='fetch_pics')
 ]
--- core/views.py
+++ core/views.py
@@ -1,4 +1,8 @@
+from django.db.models import Q
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.core import serializers
+from .models import Category, Picture
 from .utils import filter_pictures
 
 # Create your views here.
@@ -10,7 +14,25 @@ def home(request):
 
 def architecture(request):
     return filter_pictures(request, '1', 'site/architecture.html')
-
+def fetch_pics(request,type):
+    if request.method == "GET":
+        pictures = Picture.objects.filter(type=type)
+        title_filter = request.GET.get('title')
+        category_filter = request.GET.get('category')
+        if title_filter and category_filter:
+            pictures = pictures.filter(Q(title__icontains=title_filter) & Q(category__id=category_filter))
+        elif title_filter:
+            pictures = pictures.filter(title__icontains=title_filter)
+        elif category_filter:
+            pictures = pictures.filter(category__id=category_filter)
+        pictures_data = {}
+        for i, picture in enumerate(pictures):
+            picture_data = {
+                'title': picture.title,
+                'image_url': picture.image.url,
+            }
+            pictures_data[i] = picture_data
+    return JsonResponse({"pictures":pictures_data},safe=False)
 
 def advertising(request):
     return filter_pictures(request, '2', 'site/advertising.html')
